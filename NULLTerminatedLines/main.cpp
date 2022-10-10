5b32973454e43c813b6fd4052c8314f8943c02f0
NastyaSkv <NastyaSkv@DESKTOP-S0J6MVO>
2022-10-11 00:15:05 +0300

    done

--- NULLTerminatedLines/main.cpp
+++ NULLTerminatedLines/main.cpp
@@ -5,11 +5,12 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-int StringLenght(const char str[]);
+
+int StringLength(const char str[]);
 void upper_case(char str[]);
 void lower_case(char str[]);
 void shrink(char str[]);
-//void is_palindrome(char str[]);
+bool is_palindrome(char str[]);
 
 //#define EXAMPLE_1
 #define EXAMPLE_2
@@ -17,54 +18,61 @@ void shrink(char str[]);
 void main()
 {
 	setlocale(LC_ALL, "");
-
 #ifdef EXAMPLE_1
-	//char str[] = { 'H', 'e', 'l', 'l', 'o', 0};
+	//объявление строки - (1)
+	//char str[] = {'H', 'e', 'l', 'l', 'o', 0};   //в конце обязательно 0!
+	//объявление строки - (2)
 	char str[] = "Hello";
-	//str[] - строковая переменная
-	//"Hello" - строковая константа
+	// str[] - строковая переменная
+	// "Hello" - строковая константа
 	cout << str << endl;
 	cout << typeid(str).name() << endl;
 	cout << typeid("Hello").name() << endl;
-#endif 
+#endif
 
-#ifdef EXAMPLE_2
-	//for(int i = 0; i < 256; i++)cout << i << "\t" << char();
-	
-	const int SIZE = 20;
+	const int SIZE = 256;
 	char str[SIZE] = {};
-	cout << "Введите строку: "; 
-	//cin >> str;
+	cout << "Введите строку: ";
+	cin >> str;
 	SetConsoleCP(1251);
-	cin.getline(str, SIZE); //это разделитель
+	cin.getline(str, SIZE);
 	SetConsoleCP(866);
 	cout << str << endl;
-	cout << "Длина строки: " << StringLenght(str)<<endl;
-	cout << "Длина строки: " << strlen(str)<<endl; //встроенная функция
-	cout << "Длина строки в байтах: " << sizeof(str) << endl;
+	cout << "Размер строки: " << StringLength(str) << endl; //это мы сами написали
+	cout << "Размер строки: " << strlen(str) << endl; //это встроенная ф-я
+	cout << "Размер строки в байтах: " << sizeof(str) << endl; 
 	upper_case(str);
 	cout << str << endl;
 	lower_case(str);
 	cout << str << endl;
 	shrink(str);
-
-
-#endif
+	cout << str << endl;
+	cout << (is_palindrome(str)?"":"НЕ") << " является палиндромом" << endl;
+	cout << str << endl;
 }
-int StringLenght(const char str[])
+
+int StringLength(const char str[])
 {
 	int i = 0;
-	for (; str[i]; i++);  //здесь мы разыменовали последний элемент
-	return i;             //а последний элемент 0, а 0 это-False
+	//for (; str[i] != 0; i++) - мой вариант
+	for (; str[i]; i++);  //вариант О.А.
+	return(i);
 }
 
 void upper_case(char str[])
 {
+	//мой вариант
+	/*for (int i = 0; str[i]; i++)
+	{
+		str[i] = char(int(str[i]) - 32);
+		cout << str[i];
+	}*/
+
 	for (int i = 0; str[i]; i++)
 	{
-		/*if (str[i] >= 'a' && str[i] <= 'z')str[i] -= ' ';
-		if (str[i] >= 'а' && str[i] <= 'я')str[i] -= ' ';
-		if (str[i] == 'ё') str[i] = 'Ё';*/
+		/*if (str[i] >= 'a' && str[i] <= 'z')str[i] -= 32;
+		if(str[i] >= 'а' && str[i] <= 'я')str[i] -= 32;
+		if (str[i] == 'ё')str[i] = 'Ё';*/
 		str[i] = toupper(str[i]);
 	}
 }
@@ -77,10 +85,45 @@ void lower_case(char str[])
 	}
 }
 
-/*void shrink(char str[])
+void shrink(char str[])
 {
 	for (int i = 0; str[i]; i++)
 	{
-		if(str[])
+		while (str[i] == ' ' && str[i + 1] == ' ')
+		{
+			for (int j = i + 1; str[j]; j++)
+			{
+				str[j] = str[j + 1];
+			}
+		}
 	}
-}*/
+}
+
+void remove_symbol(char str[], const char symbol)
+{
+	for (int i = 0; str[i]; i++)
+	{
+		while (str[i] == symbol)
+		{
+			for (int j = i; str[j]; j++)str[j] = str[j + 1];
+		}
+	}
+}
+bool is_palindrome(char str[])
+{
+	char* buffer = new char[strlen(str) + 1]{};
+	//strcpy(buffer, str);
+	lower_case(buffer);
+	remove_symbol(buffer, ' ');
+	int size = strlen(buffer);
+	for (int i = 0; i < size; i++)
+	{
+		if (buffer[i] != buffer[size - 1 - i])
+		{
+			delete[] buffer;
+			return false;
+		}
+	}
+	delete[] buffer;
+	return true;
+}
